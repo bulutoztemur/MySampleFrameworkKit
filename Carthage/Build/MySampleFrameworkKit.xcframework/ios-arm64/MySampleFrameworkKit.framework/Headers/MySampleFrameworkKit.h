96c246dc9f8dfafdc0231f601f991fe55cc6ae77
bulut.oztemur <bulut.oztemur@adevinta.com>
2023-04-16 22:42:33 +0200

    first commit

---
+++ Carthage/Build/MySampleFrameworkKit.xcframework/ios-arm64/MySampleFrameworkKit.framework/Headers/MySampleFrameworkKit.h
@@ -0,0 +1,18 @@
+//
+//  MySampleFrameworkKit.h
+//  MySampleFrameworkKit
+//
+//  Created by bulut.oztemur on 16.04.23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MySampleFrameworkKit.
+FOUNDATION_EXPORT double MySampleFrameworkKitVersionNumber;
+
+//! Project version string for MySampleFrameworkKit.
+FOUNDATION_EXPORT const unsigned char MySampleFrameworkKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MySampleFrameworkKit/PublicHeader.h>
+
+
---
+++ MySampleFrameworkKit/MySampleVC.swift
@@ -0,0 +1,14 @@
+//
+//  MySampleVC.swift
+//  MySampleFrameworkKit
+//
+//  Created by bulut.oztemur on 16.04.23.
+//
+
+import UIKit
+
+open class MySampleVC: UIViewController {
+    public func changeBackground(withColor color: UIColor) {
+        self.view.backgroundColor = color
+    }
+}
